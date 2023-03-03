ad1a3a076860ae368a2061f591de12376c2787c1
Mikhail Sukhenko <tymah@yahoo.com>
2023-03-03 10:53:25 -0500

    fixing lint

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
-
+no-console
 app.listen(PORT, () => {
-  // eslint-disable-next-line no-use-before-define
+  // eslint-disable no-console
   console.log('server started on port 5000')
 })
