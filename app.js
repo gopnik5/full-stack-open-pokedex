23022ef092e75bbcf2564dbcc3159165827e3bed
Mikhail Sukhenko <tymah@yahoo.com>
2023-03-03 10:45:58 -0500

    fixing lint

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const app = express()
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
 
-app.use(express.static('dist'));
+app.use(express.static('dist'))
 
 app.listen(PORT, () => {
-  console.log('server started on port 5000');
-});
+  // eslint-disable-next-line no-use-before-define
+  console.log('server started on port 5000')
+})
